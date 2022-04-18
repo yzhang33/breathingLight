bb971b32ca9204fde79ca69929b5413bd0c1e4cd
yz33 <youmingzhang19@gmail.com>
2022-04-18 16:54:30 -0400

    animation

--- arduino/light_matrix.ino
+++ arduino/light_matrix.ino
@@ -1,37 +1,51 @@
+#include <FastLED.h>
 
-//VARIABLES AND DEFINES HERE - NEEDED BY THE WS2812 DRIVER CODE
-#define WS2812_pin 6 // only digital pin 8 works right now
-#define numberOfLEDs 256// total number of RGB LEDs
-byte RGB[256*3];//take your number of LEDs and multiply by 3
+#define LED_PIN 6
+#define NUM_LEDS 256
 
-// FUNCTIONS HERE
-void RGB_update(int LED, byte RED, byte GREEN, byte BLUE);//function to drive LEDs
+//byte RGB[256*3];//take your number of LEDs and multiply by 3
 
-void mapLed(int x, int y, byte RED, byte GREEN, byte BLUUE);
+CRGB leds[NUM_LEDS];
+byte levelTable[255];
+int change = 1;
+int fadeIncremet = 10;
+int currentLevel = 1; // current light level
 
 void setup() {
-  pinMode(WS2812_pin, OUTPUT);
+  //pinMode(WS2812_pin, OUTPUT);
+  Serial.begin(9600);
+  FastLED.addLeds<SK6812, LED_PIN>(leds, NUM_LEDS);
+  fillLevelTable();
 }//setup
 
-
 void loop() {
-//1
-mapLEDXY(7,7,200,200,255);mapLEDXY(7,8,200,200,255);mapLEDXY(8,7,200,200,255);mapLEDXY(8,8,200,200,255);										
-RGB_update(-1,0,0,0);
-delay(50);
-//2
-mapLEDXY(6,6,200,150,255);mapLEDXY(6,7,200,150,255);mapLEDXY(6,8,200,150,255);mapLEDXY(6,9,200,150,255);mapLEDXY(7,6,200,150,255);mapLEDXY(7,9,200,150,255);mapLEDXY(8,6,200,150,255);mapLEDXY(8,9,200,150,255);mapLEDXY(9,6,200,150,255);mapLEDXY(9,7,200,150,255);mapLEDXY(9,8,200,150,255);mapLEDXY(9,9,200,150,255);
-RGB_update(-1,0,0,0);
-delay(50);
-//3
-mapLEDXY(5,5,150,150,255);mapLEDXY(5,6,150,150,255);mapLEDXY(5,7,150,150,255);mapLEDXY(5,8,150,150,255);mapLEDXY(5,9,150,150,255);mapLEDXY(5,10,150,150,255);mapLEDXY(6,5,150,150,255);mapLEDXY(6,10,150,150,255);mapLEDXY(7,5,150,150,255);mapLEDXY(7,10,150,150,255);mapLEDXY(8,5,150,150,255);mapLEDXY(8,10,150,150,255);mapLEDXY(9,5,150,150,255);mapLEDXY(9,10,150,150,255);mapLEDXY(10,5,150,150,255);mapLEDXY(10,6,150,150,255);mapLEDXY(10,7,150,150,255);mapLEDXY(10,8,150,150,255);mapLEDXY(10,9,150,150,255);mapLEDXY(10,10,150,150,255);
-RGB_update(-1,0,0,0);
-delay(50);
-//4
-
+  clearAll();
+  //1
+  mapLEDXY(7,7,200,200,255);mapLEDXY(7,8,200,200,255);mapLEDXY(8,7,200,200,255);mapLEDXY(8,8,200,200,255);                    
+  FastLED.show();
+  delay(1000);
+  //2
+  mapLEDXY(5,7,207,60,245);mapLEDXY(6,6,207,60,245);mapLEDXY(6,7,207,60,245);mapLEDXY(6,8,207,60,245);mapLEDXY(7,5,207,60,245);mapLEDXY(7,6,207,60,245);mapLEDXY(7,8,207,60,245);mapLEDXY(7,9,207,60,245);mapLEDXY(8,5,207,60,245);mapLEDXY(8,6,207,60,245);mapLEDXY(8,8,207,60,245);mapLEDXY(8,9,207,60,245);mapLEDXY(9,6,207,60,245);mapLEDXY(9,7,207,60,245);mapLEDXY(9,8,207,60,245);mapLEDXY(10,7,207,60,245);
+  FastLED.show();
+  delay(1000);
+  //3
+  mapLEDXY(3,7,207,60,245);mapLEDXY(4,6,207,60,245);mapLEDXY(4,7,207,60,245);mapLEDXY(4,8,207,60,245);mapLEDXY(5,5,207,60,245);mapLEDXY(5,6,207,60,245);mapLEDXY(5,8,207,60,245);mapLEDXY(5,9,207,60,245);mapLEDXY(6,4,207,60,245);mapLEDXY(6,5,207,60,245);mapLEDXY(6,9,207,60,245);mapLEDXY(6,10,207,60,245);mapLEDXY(7,4,207,60,245);mapLEDXY(7,10,207,60,245);mapLEDXY(8,4,207,60,245);mapLEDXY(8,10,207,60,245);mapLEDXY(9,4,207,60,245);mapLEDXY(9,5,207,60,245);mapLEDXY(9,9,207,60,245);mapLEDXY(9,10,207,60,245);mapLEDXY(10,5,207,60,245);mapLEDXY(10,6,207,60,245);mapLEDXY(10,8,207,60,245);mapLEDXY(10,9,207,60,245);mapLEDXY(11,6,207,60,245);mapLEDXY(11,7,207,60,245);mapLEDXY(11,8,207,60,245);mapLEDXY(12,7,207,60,245);
+  FastLED.show();
+  delay(1000);
+  //
+  clearAll();
+  mapLEDXY(1,1,207,60,245);mapLEDXY(1,5,207,60,245);mapLEDXY(2,2,207,60,245);mapLEDXY(2,4,207,60,245);mapLEDXY(3,3,207,60,245);mapLEDXY(4,2,207,60,245);mapLEDXY(4,4,207,60,245);mapLEDXY(4,12,207,60,245);mapLEDXY(4,13,207,60,245);mapLEDXY(4,14,207,60,245);mapLEDXY(5,1,207,60,245);mapLEDXY(5,5,207,60,245);mapLEDXY(5,9,207,60,245);mapLEDXY(5,12,207,60,245);mapLEDXY(5,14,207,60,245);mapLEDXY(6,8,207,60,245);mapLEDXY(6,9,207,60,245);mapLEDXY(6,12,207,60,245);mapLEDXY(6,14,207,60,245);mapLEDXY(7,7,207,60,245);mapLEDXY(7,8,207,60,245);mapLEDXY(7,9,207,60,245);mapLEDXY(7,12,207,60,245);mapLEDXY(7,14,207,60,245);mapLEDXY(8,8,207,60,245);mapLEDXY(8,9,207,60,245);mapLEDXY(8,12,207,60,245);mapLEDXY(8,14,207,60,245);mapLEDXY(9,1,207,60,245);mapLEDXY(9,5,207,60,245);mapLEDXY(9,9,207,60,245);mapLEDXY(9,12,207,60,245);mapLEDXY(9,14,207,60,245);mapLEDXY(10,2,207,60,245);mapLEDXY(10,4,207,60,245);mapLEDXY(10,12,207,60,245);mapLEDXY(10,13,207,60,245);mapLEDXY(10,14,207,60,245);mapLEDXY(11,3,207,60,245);mapLEDXY(12,2,207,60,245);mapLEDXY(12,4,207,60,245);mapLEDXY(13,1,207,60,245);mapLEDXY(13,5,207,60,245);
+  FastLED.show();
+  delay(1000);
 }//loop
 
-void mapLed(int x, int y, byte RED, byte GREEN, byte BLUE){
+void clearAll(){
+  for(int i=0;i<NUM_LEDS;i++){
+    leds[i] =  CRGB (0,0,0);
+  }
+}
+
+void mapLEDXY(int x, int y, byte RED, byte GREEN, byte BLUE){
     int RGBlocation = 0;
     if(y % 2 == 0){
         RGBlocation = x + y * 16;
@@ -39,90 +53,19 @@ void mapLed(int x, int y, byte RED, byte GREEN, byte BLUE){
         RGBlocation = 15 - x + y * 16;
     }
     //update rgb
-    RGB[RGBlocation*3] = BLUE;
-    RGB[RGBlocation*3 + 1] = RED;
-    RGB[RGBlocation*3 + 2] = GREEN;
+    Serial.println(RGBlocation);
+    leds[RGBlocation] =  CRGB (RED,GREEN,BLUE);
+    //leds[RGBlocation].fadeLightBy(levelTable[currentLevel]);
 }
 
-
-//WS2812 Driver Function
-void RGB_update(int LED, byte RED, byte GREEN, byte BLUE) {
-  // LED is the LED number starting with 0
-  // RED, GREEN, BLUE is the brightness 0..255 setpoint for that LED
-  byte ExistingPort, WS2812pinHIGH;//local variables here to speed up pinWrites
-  
-  if(LED>=0){//map the REG GREEN BLUE Values into the RGB[] array
-  RGB[LED * 3] = GREEN;
-  RGB[LED * 3 + 1] = RED;
-  RGB[LED * 3 + 2] = BLUE;
+void fillLevelTable(){
+   float maxValue = 255;
+  // iterate over the array and calculate the right value for it:
+  for (int l = 0; l <= maxValue; l++) {
+    // map input to a 0-179 range:
+    float angle = map(l, 0, maxValue, 0, 179);
+    //here it all is in one line:
+    float lightLevel = (sin((angle * PI / 180) + PI / 2) + 1) * 127.5;
+    levelTable[l] = lightLevel;
   }
-  
-  noInterrupts();//kill the interrupts while we send the bit stream out...
-  ExistingPort = PORTB; // save the status of the entire PORT B - let's us write to the entire port without messing up the other pins on that port
-  WS2812pinHIGH = PORTB | 1; //this gives us a byte we can use to set the whole PORTB with the WS2812 pin HIGH
-  int bitStream = numberOfLEDs * 3;//total bytes in the LED string
-
-//This for loop runs through all of the bits (8 at a time) to set the WS2812 pin ON/OFF times
-  for (int i = 0; i < bitStream; i++) {
-
-    PORTB = WS2812pinHIGH;//bit 7  first, set the pin HIGH - it always goes high regardless of a 0/1 
-    
-    //here's the tricky part, check if the bit in the byte is high/low then right that status to the pin
-    // (RGB[i] & B10000000) will strip away the other bits in RGB[i], so here we'll be left with B10000000 or B00000000
-    // then it's easy to check if the bit is high or low by AND'ing that with the bit mask ""&& B10000000)"" this gives 1 or 0
-    // if it's a 1, we'll OR that with the Existing port, thus keeping the pin HIGH, if 0 the pin is written LOW
-    PORTB = ((RGB[i] & B10000000) && B10000000) | ExistingPort; 
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");//these are NOPS - these let us delay clock cycles for more precise timing 
-    PORTB = ExistingPort;//okay, here we know we have to be LOW regardless of the 0/1 bit state
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");//minimum LOW time for pin regardless of 0/1 bit state
-
-    // then do it again for the next bit and so on... see the last bit though for a slight change
-
-    PORTB = WS2812pinHIGH;//bit 6
-    PORTB = ((RGB[i] & B01000000) && B01000000) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 5
-    PORTB = ((RGB[i] & B00100000) && B00100000) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 4
-    PORTB = ((RGB[i] & B00010000) && B00010000) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 3
-    PORTB = ((RGB[i] & B00001000) && B00001000) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 2
-    PORTB = ((RGB[i] & B00000100) && B00000100) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 1
-    PORTB = ((RGB[i] & B00000010) && B00000010) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-    PORTB = ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t");
-
-    PORTB = WS2812pinHIGH;//bit 0
-    __asm__("nop\n\t");//on this last bit, the check is much faster, so had to add a NOP here
-    PORTB = ((RGB[i] & B00000001) && B00000001) | ExistingPort;
-    __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"); 
-    PORTB = ExistingPort;//note there are no NOPs after writing the pin LOW, this is because the FOR Loop uses clock cycles that we can use instead of the NOPS
-  }//for loop
-
-  
-  interrupts();//enable the interrupts
-
-// all done!
-}
+} 
